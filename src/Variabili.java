f2a96eb80fe2d961f6f3379d15234d94ef1b441d
11292-stella <stella.marucelli@gmail.com>
2025-05-05 15:41:15 +0200

    metodo stringa

--- src/Variabili.java
+++ src/Variabili.java
@@ -10,9 +10,18 @@ public class Variabili {
         int b = scanner.nextInt();
         moltiplica(a,b);
 
-        String t = "benvenuto";
-        t = t + " Topo Gigio";
-        System.out.println(t);
+        Scanner scanner1 = new Scanner(System.in);
+        System.out.println("scrivi un nome");
+
+        String t = scanner1.nextLine();
+        System.out.println("scrivi un altro nome");
+        String c = scanner1.nextLine();
+
+        stringa("benvenuto " + t , c);
+    }
+
+    public static void stringa(String t, String c){
+        System.out.println(t+c);
     }
 
     public static void moltiplica(int a, int b){
