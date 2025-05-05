3356ee3ff91592d3957b462e27f6eeff7e9eb7b7
11292-stella <stella.marucelli@gmail.com>
2025-05-05 15:21:07 +0200

    metodo moltiplica

---
+++ src/Variabili.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Variabili {
+    public static void main(String[] args){
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("dammi un numero");
+        int a = scanner.nextInt();
+        System.out.println("dammi un secondo numero");
+        int b = scanner.nextInt();
+        moltiplica(a,b);
+    }
+
+    public static void moltiplica(int a, int b){
+        System.out.println(a*b);
+    }
+}
