7dcd1062cbdbd74f05e461cf08294f61885ed377
11292-stella <stella.marucelli@gmail.com>
2025-05-05 16:26:46 +0200

    altro

--- src/Esercizio1.java
+++ src/Esercizio1.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Variabili {
+public class Esercizio1 {
     public static void main(String[] args){
 
         Scanner scanner = new Scanner(System.in);
@@ -18,7 +18,34 @@ public class Variabili {
         String c = scanner1.nextLine();
 
         stringa("benvenuto " + t , c);
+
+        String[] array = {"primo","secondo","terzo","quarto","quinto"};
+        String nuova = "sesto";
+
+        String[] risultato = inseriamoTerzoPosto(array,nuova);
+
+        for (String s : risultato){
+            System.out.println(s + " ");
+        }
+
+
     }
+
+    public static String[] inseriamoTerzoPosto(String[]arrayOriginal,String nuovaStringa ){
+        String[] nuovoArray = new String[6];
+
+        nuovoArray[0] = arrayOriginal[0];
+        nuovoArray[1] = arrayOriginal[1];
+        nuovoArray[2] = nuovaStringa;
+        nuovoArray[3] = arrayOriginal[2];
+        nuovoArray[4] = arrayOriginal[3];
+        nuovoArray[5] = arrayOriginal[4];
+
+        return nuovoArray;
+
+    }
+
+
 
     public static void stringa(String t, String c){
         System.out.println(t+c);
